12fb42c4e1aa02e683b2830e043687b194202db9
Christos Stefanis <xristostefanis@gmail.com>
2023-05-01 12:33:53 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-6xl font-bold text-slate-900">ThetaRealm</h1>
+      <h3 className="text-2xl mt-8 text-slate-900">
+        Next.js, TailwindCSS, Solidity, IPFS, The Graph and Theta Video API.
+      </h3>
+    </div>
+  );
+}
